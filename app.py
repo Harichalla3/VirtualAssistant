8ac9b0db22a0f08ec8a54456f0d2f94f6282ea56
THECHALLA <147300897+Harichalla3@users.noreply.github.com>
2024-07-17 14:29:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-import speech_recognition as sr
 import pyttsx3
 import pywhatkit
 import datetime
@@ -7,7 +6,6 @@ import wikipedia
 
 app = Flask(__name__)
 
-listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
@@ -15,20 +13,6 @@ engine.setProperty('voice', voices[1].id)
 def talk(text):
     engine.say(text)
     engine.runAndWait()
-
-def take_command():
-    try:
-        with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'sweety' in command:
-                command = command.replace('sweety', '')
-                print(command)
-    except:
-        pass
-    return command
 
 def run_sweety(command):
     if 'play' in command:
